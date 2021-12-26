32bac3b43bf6d1078eaf3a9c3fb154849a21feb2
Shivam Kumar <kumar.shivam.jarvis@gmail.com>
2021-12-26 12:40:49 +0530

    [keymaps] Better window resizing in normal mode
    - Ctrl + Left -> Resize to left
    - Ctrl + Down -> Resize to down
    - Ctrl + Up -> Resize to up
    - Ctrl + Right -> Resize to right
    
    Signed-off-by: Shivam Kumar <kumar.shivam.jarvis@gmail.com>

--- lua/shivam/keymaps.lua
+++ lua/shivam/keymaps.lua
@@ -16,3 +16,9 @@ keymap("n", "<C-h>", "<C-w>h", opts)            -- Ctrl + h -> Move to left wind
 keymap("n", "<C-j>", "<C-w>j", opts)            -- Ctrl + j -> Move to lower window
 keymap("n", "<C-k>", "<C-w>k", opts)            -- Ctrl + k -> Move to upper window
 keymap("n", "<C-l>", "<C-w>l", opts)            -- Ctrl + l -> Move to right window
+
+-- Resize with arrows
+keymap("n", "<C-Up>", ":resize -2<CR>", opts)               -- Ctrl + Up -> Resize to up
+keymap("n", "<C-Down>", ":resize +2<CR>", opts)             -- Ctrl + Down -> Resize to down
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)    -- Ctrl + Left -> Resize to left
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)   -- Ctrl + Right -> Resize to right

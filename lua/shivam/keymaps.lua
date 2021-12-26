32c476df755c2e7adf3530d6373bb43637806d25
Shivam Kumar <kumar.shivam.jarvis@gmail.com>
2021-12-26 12:33:09 +0530

    [keymaps] Better window navigation in normal mode
    - Ctrl + h -> Move to left window
    - Ctrl + j -> Move to lower window
    - Ctrl + k -> Move to upper window
    - Ctrl + l -> Move to right window
    
    Signed-off-by: Shivam Kumar <kumar.shivam.jarvis@gmail.com>

--- lua/shivam/keymaps.lua
+++ lua/shivam/keymaps.lua
@@ -8,3 +8,11 @@ keymap("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+
+-- Normal Mode --
+
+-- Better Window Navigation
+keymap("n", "<C-h>", "<C-w>h", opts)            -- Ctrl + h -> Move to left window
+keymap("n", "<C-j>", "<C-w>j", opts)            -- Ctrl + j -> Move to lower window
+keymap("n", "<C-k>", "<C-w>k", opts)            -- Ctrl + k -> Move to upper window
+keymap("n", "<C-l>", "<C-w>l", opts)            -- Ctrl + l -> Move to right window

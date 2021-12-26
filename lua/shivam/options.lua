d743f7d9e09b1796a4786e8ab6a628b0e2bc4c02
Shivam Kumar <kumar.shivam.jarvis@gmail.com>
2021-12-26 11:33:19 +0530

    [options] Modify some tab and indent options
    - Enable smart tab
    - Enable Smart indent
    - Set tab and indent to 4 spaces
    - Convert taba to spaces
    
    Signed-off-by: Shivam Kumar <kumar.shivam.jarvis@gmail.com>

--- lua/shivam/options.lua
+++ lua/shivam/options.lua
@@ -1,6 +1,11 @@
 local options = {
     number = true,                -- Enable numbered lines
     numberwidth = 4,              -- Set number column width
+    expandtab = true,             -- Convert tabs to spaces
+    smartindent = true,           -- Enable smart indenting
+    shiftwidth = 4,		  -- Number of spaces inserted for each indentation
+    tabstop = 4,		  -- Number of spaces inserted for each tab
+    smarttab = true,		  -- Enable smart tabbing
 }
 
 for k, v in pairs(options) do

5f8302fbfe5471ca102dca25f12e14b93e6803f2
Shivam Kumar <kumar.shivam.jarvis@gmail.com>
2022-01-08 21:44:16 +0530

    Add Treesitter
    Signed-off-by: Shivam Kumar <kumar.shivam.jarvis@gmail.com>

--- lua/shivam/plugins.lua
+++ lua/shivam/plugins.lua
@@ -52,6 +52,9 @@ return packer.startup(function(use)
     use 'nvim-telescope/telescope.nvim' -- Find, Filter, Preview, Pick
     use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' } -- FZF sorter for telescope written in c
 
+    -- Treesitter
+  use {"nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }  -- Nvim Treesitter configurations and abstraction layer
+
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
     if PACKER_BOOTSTRAP then

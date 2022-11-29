194b5c59bf0841fb7374aca7482fa4e8c6329c1e
Shivam Kumar <kumar.shivam.jarvis@gmail.com>
2022-11-29 15:50:57 +0530

    [mason-lspconfig] Enable Automatic LSP Setup
    Signed-off-by: Shivam Kumar <kumar.shivam.jarvis@gmail.com>

--- lua/shivam/mason-lspconfig.lua
+++ lua/shivam/mason-lspconfig.lua
@@ -6,3 +6,13 @@ end
 mason_lspconfig.setup({
   ensure_installed = {}
 })
+
+mason_lspconfig.setup_handlers {
+    -- The first entry (without a key) will be the default handler
+    -- and will be called for each installed server that doesn't have
+    -- a dedicated handler.
+    function (server_name) -- default handler (optional)
+        require("lspconfig")[server_name].setup {}
+    end,
+    -- Next, you can provide a dedicated handler for specific servers.
+}
